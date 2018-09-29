fba2989842964063c21716eab5d9386225eb366e
varsh18 <varshiniravi1824@gmail.com>
2018-09-29 21:09:37 +0530

    Half Pyramid using star

---
+++ Half Pyramid.cpp
@@ -0,0 +1,20 @@
+/*
+Half Pyramid using *
+*
+* *
+* * *
+* * * *
+* * * * *
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int i,j,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	for(j=0;j<=i;j++)
+	printf("* ");
+	printf("\n");
+	}
+}
