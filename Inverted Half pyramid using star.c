7143df7d7eaf6a02a62666185a5749dcba0c3049
varsh18 <varshiniravi1824@gmail.com>
2018-10-05 09:15:51 +0530

    Add Inverted half Pyramid using star

---
+++ Inverted Half pyramid using star.c
@@ -0,0 +1,22 @@
+/*
+
+Inverted Half Pyramid using *
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	 int i,j,n;
+	 scanf("%d",&n);
+	 for(i=0;i<n;i++)
+	 {
+		for(j=0;j<n-i;j++)
+		printf("* ");
+		printf("\n");
+	 }
+}
