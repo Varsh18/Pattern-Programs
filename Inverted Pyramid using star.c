48321645c516505b80764b3359bd5f9f80c4b027
varsh18 <varshiniravi1824@gmail.com>
2018-10-05 09:36:33 +0530

    Add Inverted Pyramid using star

---
+++ Inverted Pyramid using star.c
@@ -0,0 +1,29 @@
+/*
+
+Inverted Full pyramid using star
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	 int i,j,n;
+	 scanf("%d",&n);
+	 for(i=0;i<n;i++)
+	 {
+	 	//loop for spaces
+	 	for(j=0;j<i;j++)
+	 	printf("  ");
+	 	//loop for first half of the triangle
+	 	for(j=0;j<n-i;j++)
+		printf("* ");
+		//loop for second half of the triangle
+		for(j=0;j<n-i-1;j++)
+		printf("* ");
+		printf("\n");
+	 }
+}
